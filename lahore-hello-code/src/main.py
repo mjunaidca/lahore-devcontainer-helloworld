1db07190dc8e723e92abbb28e7d0ab2bd1b068f4
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2024-05-12 11:02:14 +0500

    restful api call

--- lahore-hello-code/src/main.py
+++ lahore-hello-code/src/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
 
+def my_first_api_call():
+    call_api = requests.get("http://order-api:8000/")
+    return call_api.json()
+
+
 @app.get("/")
 def read_root():
-    return {"Multiple MicrosefvicesCool Just Added Volume!!": "5 MAY"}
+    api_call = my_first_api_call()
+    return {"Multiple Microsefvices!!": api_call}
